8186dcd621879c801637a3d82a1677d4215256dd
cjosu <88686663+cjosu@users.noreply.github.com>
2022-07-20 13:57:36 -0400

    Created app provider
    This component passes global state, routes, query cache, etc to all child components

---
+++ client/src/providers/app.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalClient } from 'store/GlobalClient';
+
+export const queryClient = new QueryClient();
+
+type AppProviderProps = {
+  children: React.ReactNode;
+}
+
+export const AppProvider = ({ children }: AppProviderProps) => {
+  return (
+    <div className="app-container">
+      <GlobalClient>
+        <QueryClientProvider client={queryClient}>
+          <Router>
+            {children}
+          </Router>
+        </QueryClientProvider>
+      </GlobalClient>
+    </div>
+  )
+}
+
