dfd52521fae3b6bec692c2462254c599ed9180cb
jmviz <jmviz@jmviz.dev>
2023-09-07 21:01:54 -0400

    create-react-app bootstrap

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+
+function App() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <div className="App">
+        <header className="App-header"></header>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
