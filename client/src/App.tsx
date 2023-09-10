e516b5a4920a3ffcb7b9125f6c9f9d36ec303508
jmviz <jmviz@jmviz.dev>
2023-09-09 22:36:10 -0400

    search wip

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,28 @@
 import "./App.css";
+import SearchPane from "./SearchPane";
 
-import React from "react";
+import { Fragment, useState } from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 
 function App() {
+  const [langId, setLangId] = useState<number | null>(null);
+  // const [includeLangIds, setIncludeLangIds] = useState<number[]>([]);
+  // const [termId, setTermId] = useState<number | null>(null);
+
   return (
-    <React.Fragment>
+    <Fragment>
       <CssBaseline />
       <div className="App">
-        <header className="App-header"></header>
+        <header className="App-header">
+          <SearchPane
+            langId={langId}
+            setLangId={setLangId}
+            // setIncludeLangIds={setIncludeLangIds}
+            // setTermId={setTermId}
+          />
+        </header>
       </div>
-    </React.Fragment>
+    </Fragment>
   );
 }
 
